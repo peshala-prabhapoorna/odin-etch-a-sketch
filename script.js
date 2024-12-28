3b2f94fcc3c6c4a47b586a2c66bc390f34526015
peshala-prabhapoorna <peshalaprabhapoorna@gmail.com>
2024-12-28 16:04:53 +0530

    feat: add reset button

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-const DEFAULT_GRID_SIZE = 16;
+const DEFAULT_GRID_SIZE = 32;
+const MAX_GRID_SIZE = 100;
 
-const grid = document.querySelector('#grid');
+drawGrid(DEFAULT_GRID_SIZE);
 
-for (let i = 0; i < DEFAULT_GRID_SIZE; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+    let gridSize = prompt('Set the grid size:', DEFAULT_GRID_SIZE);
+    if (gridSize > MAX_GRID_SIZE) gridSize = MAX_GRID_SIZE;
+    drawGrid(gridSize);
+});
 
-    for (let j = 0; j < DEFAULT_GRID_SIZE; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
+function drawGrid(gridSize) {
+    const grid = document.querySelector('#grid');
+    grid.innerHTML = '';
+
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j < gridSize; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+
+        grid.appendChild(row)
     }
 
-    grid.appendChild(row)
+    grid.addEventListener('mouseover', (event) => {
+        const targetCell = event.target;
+        targetCell.classList.add('highlight');
+    });
 }
-
-grid.addEventListener('mouseover', (event) => {
-    const targetCell = event.target;
-    targetCell.classList.add('highlight');
-});
